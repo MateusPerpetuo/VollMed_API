bd3e1c6ff911919e73e617bcd1350f14d11e20a5
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-07-12 08:30:57 -0300

    v 0.005 Halitado o metodo GET para listar os medicos cadastrados no BD e com paginação e por ordem alfabética

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -2,11 +2,17 @@ package med.vol.api.controller;
 
 import jakarta.validation.Valid;
 import med.vol.api.medico.DadosCadastroMedico;
+import med.vol.api.medico.DadosListagemMedico;
 import med.vol.api.medico.Medico;
 import med.vol.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,5 +25,15 @@ public class MedicoController {
     @Transactional // tem que ser a anotação que vem do pacote do string
     public void cadastrar( @RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping                 //Pageable tem que ser uma classe do pacote Spring e nao awt
+    public Page<DadosListagemMedico> lisstarMedicos(
+            @PageableDefault(size = 10, sort = {"nome"})
+           // @RequestParam(required = false)
+            Pageable paginacao) {
+
+        return repository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
     }
 }
