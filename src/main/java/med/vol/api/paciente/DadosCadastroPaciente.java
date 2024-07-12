489b6701881b78a003b546508f57fcc0e978a22b
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-07-12 08:44:58 -0300

    v 0.006 Criação de Entidade, Classe e tabela DB dos Pacientes

---
+++ src/main/java/med/vol/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,26 @@
+package med.vol.api.paciente;
+
+import jakarta.persistence.Column;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.vol.api.endereco.Endereco;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @CPF
+        @Column(unique = true)
+        String cpf,
+        @NotNull
+        @Valid
+        Endereco endereco ) {
+
+}
