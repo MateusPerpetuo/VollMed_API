ae9da7d9e995152b5de7d16a3f8c918f08982c00
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-07-13 19:35:34 -0300

    v 0.0020 - Crianto o filtro de segurança

---
+++ src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -0,0 +1,22 @@
+package med.vol.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// @Component é utilizado para que o Spring carregue uma classe / componente genérico
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // Linha responsável por seguir para o proximo filtro ou continuar a aplicação
+        filterChain.doFilter(request, response);
+    }
+}
